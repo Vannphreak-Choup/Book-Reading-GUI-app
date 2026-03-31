7d4dc8235032f792569508d5d9a8ec30a8da8034
Vannphyreak Choup <choupvannphyreak@gmail.com>
2026-03-31 14:51:54 +0700

    design the zoom button and add functionality to the remove button and add comment to all the code

--- src/main.py
+++ src/main.py
@@ -20,12 +20,6 @@ app.iconbitmap(app_icon)
 # the size of the app when not open fullscreen
 app.geometry("1200x600")
 
-# the default zoom level when open a pdf file
-zoom_level = 1.0
-# initialize the current page to 0 when open a pdf file
-current_page = 0
-# a variable to track the generation of the render, we will use it to cancel the render when the user open a new pdf file or zoom in/out
-render_generation = 0
 
 # the font and size we use for the app
 my_font = ctk.CTkFont(family="Arial", size=15, weight="bold")
@@ -57,15 +51,23 @@ def select_file(filename):
     
     print(f"selected file: {filename}")
 
+# the default zoom level when open a pdf file
+zoom_level = 1.0
+
+# when user click this button it increase the zoom level by 0.2
 def zoom_in():
     global zoom_level
     zoom_level += 0.2
     render_all_page()
 
+# when user click this button it decrease the zoom level by 0.2, but it will not go beyond 0.4 because we don't want the page to be too small
 def zoom_out():
     global zoom_level
     zoom_level = max(0.4, zoom_level - 0.2)
     render_all_page()
+
+# initialize the current page to 0 when open a pdf file
+current_page = 0
 
 # when user click open it checks if there is a file to select
 def open_pdf():
@@ -82,6 +84,26 @@ def open_pdf():
 
     render_all_page()
 
+# when user click remove it check if there is a file to select
+def remove_pdf():
+    if not data.selected_files:
+        print("No file selected")
+        return
+    # if so:
+    # remove the file from the data
+    del data.pdf_files[data.selected_files]
+    # remove the label from the file list
+    data.file_labels[data.selected_files].destroy()
+    # remove the label from the data
+    del data.file_labels[data.selected_files]
+    # clear the selected file
+    data.selected_files = None
+
+    # clear the pdf container
+    for widget in pdf_containter.winfo_children():
+        widget.destroy()
+
+# when user click zoom in/out it will call this function to render the page with the new zoom level
 def render_all_page_thread(my_generation):
     # if the document is not open, return
     if data.doc is None:
@@ -105,6 +127,7 @@ def render_all_page_thread(my_generation):
     except Exception as e:
         print(f"Thread error: {e}")
 
+# display the page on the pdf container, and check the generation before displaying the page
 def display_page(img, page_num, my_generation):
     if my_generation != render_generation:
         return
@@ -123,6 +146,9 @@ def display_page(img, page_num, my_generation):
     )
     label.pack(pady=10)
 
+# a variable to track the generation of the render, we will use it to cancel the render when the user open a new pdf file or zoom in/out
+render_generation = 0
+
 # render the current page of the pdf file
 def render_all_page():
     global render_generation
@@ -133,17 +159,20 @@ def render_all_page():
         widget.destroy()
     data.pdf_images.clear()
 
+    # start a new thread to render the page, so it will not block the main thread and the UI will still be responsive, 
+    # we pass the current generation to the thread so it can check if the generation is still the same before displaying the page
     threading.Thread(
         target=render_all_page_thread,
         args=(my_generation,),
         daemon=True
     ).start()
 
+'''All the line below are the main UI'''
 # the 3 buttons frame on top
 top_frame = ctk.CTkFrame(app)
 top_frame.pack(fill="x", padx=10, pady=5)
 
-# Add pdf 
+# Add pdf button
 addImage = "assets/icon/add.png"        
 AddPDF = Image.open(addImage)           
 Add_icon = ctk.CTkImage(
@@ -163,7 +192,7 @@ btn_add = ctk.CTkButton(
 )
 btn_add.pack(side="left", padx=5)
 
-# Open
+# Open button
 openImage = "assets/icon/open.png"      
 Open = Image.open(openImage)
 Open_icon = ctk.CTkImage(
@@ -182,7 +211,7 @@ btn_open = ctk.CTkButton(
 )
 btn_open.pack(side="left", padx=5)
 
-# Remove
+# Remove button
 removeImage = "assets/icon/remove.png"
 remove = Image.open(removeImage)
 remove_icon = ctk.CTkImage(
@@ -195,22 +224,43 @@ btn_remove = ctk.CTkButton(
     top_frame,
     text="Remove",
     image=remove_icon,
-    compound="left", 
+    compound="left",
+    command=remove_pdf, 
     font=my_font
 )
 btn_remove.pack(side="left", padx=5)
 
+# Zoom in button
+zoomin = "assets/icon/zoom_in.png"
+zoomIN = Image.open(zoomin)
+zoom_in_icon = ctk.CTkImage(
+    light_image=zoomIN,
+    dark_image=zoomIN,
+    size=(20, 20)
+)
 btn_zoom_in = ctk.CTkButton(
     top_frame,
-    text="Zoom +",
+    text="Zoom",
+    image=zoom_in_icon,
+    compound="left",
     command=zoom_in,
     font=my_font
 )
 btn_zoom_in.pack(side="right", padx=5)
 
+# Zoom out button
+zoomout = "assets/icon/zoom_out.png"
+zoomOUT = Image.open(zoomout)
+zoom_out_icon = ctk.CTkImage(
+    light_image=zoomOUT,
+    dark_image=zoomOUT,
+    size=(20, 20)
+)
 btn_zoom_out = ctk.CTkButton(
     top_frame,
-    text="Zoom -",
+    text="Zoom",
+    image=zoom_out_icon,
+    compound="left",
     command=zoom_out,
     font=my_font
 )
@@ -241,5 +291,6 @@ pdf_containter = ctk.CTkScrollableFrame(right_frame)
 pdf_containter.pack(fill="both", expand=True)
 
 
-# open the app
-app.mainloop()
+# open the app (i could've used just app.mainloop() but if i added __name__ == "__main__" it looks cooler lol)
+if __name__ == "__main__":
+    app.mainloop()
